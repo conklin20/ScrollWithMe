c041d56544cd46ea440d31f8699d96a558494fef
Cary Conklin <cary.conklin.20@gmail.com>
2019-01-30 04:26:35 +0000

    intro and elevator pitch ux update, mimicking typing

--- public/js/app.js
+++ public/js/app.js
@@ -19,5 +19,45 @@ $(document).ready(function(){
     context: '#context'
   });
   
-  // loadIntro()
+  loadIntro()
 });
+
+
+function loadIntro(){
+  $('#intro').css({
+      fontSize: 48,
+      fontFamily: 'system-ui'
+  }); 
+  let intro = $('#intro').data('intro').split('');
+  let randDelay = 1000;
+  (function theLoop (i) {
+    setTimeout(function () {
+      $('#intro').append(intro[i])
+      if (i <= intro.length) {
+        i++;        
+        theLoop(i);       
+      }
+    }, randDelay);
+    randDelay = (i % 2 === 0 ? randDelay = Math.random() * (150 - 0) + 0 : randDelay)
+  })(0)
+  loadElevator();
+}
+
+function loadElevator(){
+  $('#elevator').css({
+      fontSize: 48,
+      fontFamily: 'system-ui'
+  });
+  let elevator = $('#elevator').data('elevator').split('');
+  let randDelay = 6000;
+  (function theLoop (i) {
+    setTimeout(function () {
+      $('#elevator').append(elevator[i])
+      if (i <= elevator.length) {
+        i++;        
+        theLoop(i);       
+      }
+    }, randDelay);
+    randDelay = (i % 2 === 0 ? randDelay = Math.random() * (150 - 0) + 0 : randDelay)
+  })(0)
+}
